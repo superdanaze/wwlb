725ffef2a5a9baaf3540868d3af3ed34587e0659
Dan Jordan <superdanaze@gmail.com>
2022-03-23 08:01:35 -0700

    swapped FA icon for SVG

--- assets/themes/_wwlb/functions.php
+++ assets/themes/_wwlb/functions.php
@@ -341,12 +341,9 @@ function ethosLA_enqueue_login_items() {
 #-----------------------------------------------------------------#
 
 function ethosLA_enqueue_scripts() {
-	// wp_enqueue_script('font-awesome', get_stylesheet_directory_uri() . '/js/fontawesome-all.min.js',array(),null,true);
-	// wp_enqueue_script('fa-brands', get_stylesheet_directory_uri() . '/js/fa-brands.min.js',array(),null,true);
-	// wp_enqueue_script('fa-light', get_stylesheet_directory_uri() . '/js/fa-light.min.js',array(),null,true);
 	wp_enqueue_script('wwlb-ui', 'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js',array(),null,true);
 	wp_enqueue_script( NEW_CLIENT . "-script", get_stylesheet_directory_uri() . "/js/script.js",array(),null,true);
-	wp_enqueue_script('fa-all', get_stylesheet_directory_uri() . '/js/fa-all.min.js',array(),null,true);
+	wp_enqueue_script('fa-all', get_stylesheet_directory_uri() . '/js/fontawesome-all.min.js',array(),null,true);
 }
 
 add_action( 'wp_enqueue_scripts', 'ethosLA_enqueue_scripts' );
@@ -562,15 +559,22 @@ class ELA_Mods {
 
 
 	public function language_select() {
-
-		//	icon
+		// //	icon
 		$output = genesis_markup(
 			[
-				'open'		=> '<i %s>',
+				'open'		=> '<svg %s>',
 				'context'	=> 'language_select_icon',
-				'atts'		=> [ 'class' => "fal fa-fw fa-glasses-alt nopoint" ],
+				'atts'		=> [ 
+						'class' => "svg-inline--fa fa-glasses-alt fa-w-18 fa-fw nopoint",
+						'aria-hidden'	=> true,
+						'focusable'		=> false,
+						'data-prefix'	=> "fal",
+						'data-icon'		=> "glasses-alt",
+						'viewBox'		=> "0 0 576 512"
+					],
+				'content'	=> '<path fill="#fff" d="M572.39 302.63L528.75 92.78c-5.41-21.73-19.66-40.23-39.12-50.92-19.5-10.7-42.72-12.81-63.81-5.75l-22.85 7.61c-4.19 1.4-6.46 5.93-5.06 10.12l5.06 15.18c1.4 4.19 5.93 6.46 10.12 5.06l20.28-6.77c13.11-4.37 27.75-4.29 39.99 2.14 12.3 6.46 20.94 17.46 24.29 30.9l30.45 140.84C504.32 220.78 473.79 208 440 208c-66.89 0-122.21 48.38-133.58 112h-36.84C258.21 256.38 202.89 208 136 208c-33.79 0-64.32 12.78-88.1 33.18l30.45-140.84c3.36-13.44 11.99-24.44 24.29-30.9 12.24-6.43 26.88-6.51 39.99-2.14l20.28 6.77c4.19 1.4 8.72-.87 10.12-5.06l5.06-15.18c1.4-4.19-.87-8.72-5.06-10.12l-22.85-7.61c-21.09-7.06-44.31-4.95-63.81 5.75-19.46 10.7-33.71 29.2-39.12 50.93L3.61 302.63A172.5 172.5 0 0 0 0 336v8c0 75.11 60.89 136 136 136 72.37 0 130.97-56.69 135.19-128h33.61c4.22 71.31 62.82 128 135.19 128 75.11 0 136-60.89 136-136v-8c-.1-11.21-1.32-22.39-3.6-33.37zM136 448c-57.35 0-104-46.65-104-104s46.65-104 104-104 104 46.65 104 104-46.65 104-104 104zm304 0c-57.35 0-104-46.65-104-104s46.65-104 104-104 104 46.65 104 104-46.65 104-104 104z"></path>',
 				'echo'		=> false,
-				'close'		=> '</i>',
+				'close'		=> '</svg>',
 			]
 		);
 
